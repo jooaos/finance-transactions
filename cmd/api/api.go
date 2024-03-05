53f952527c42552217aa95804bfdef1653fd9e68
João Victor Silva Soares <joao.soares@gamersclub.com.br>
2024-03-04 23:24:50 -0400

    feat: up server and added healthz route to it

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -2,12 +2,17 @@ package main
 
 import (
 	"fmt"
-	"time"
+
+	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
-	for {
-		fmt.Println("Testando...")
-		time.Sleep(10 * time.Second)
-	}
+	app := fiber.New()
+	fmt.Printf("Intializing server...")
+
+	app.Get("/healthz", func(c *fiber.Ctx) error {
+		return c.JSON("Ok")
+	})
+
+	app.Listen(":8080")
 }
