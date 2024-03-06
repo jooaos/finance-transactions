a4ac854fd3db0ee0fb84024f73c4fdcd133d5956
João Victor Silva Soares <jovictors.soares@gmail.com>
2024-03-06 14:21:55 -0400

    feat: create transaction model

---
+++ internal/models/transactions.go
@@ -0,0 +1,8 @@
+package models
+
+type Transactions struct {
+	ID              int     `json:"id"`
+	AccountId       int     `json:"account_id"`
+	OperationTypeId int     `json:"operation_type_id"`
+	Amount          float32 `json:"amount"`
+}
