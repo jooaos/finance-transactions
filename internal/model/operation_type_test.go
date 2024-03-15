e03c44176b4db9f20268f55b79cde2e77e43b55b
João Victor Silva Soares <jovictors.soares@gmail.com>
2024-03-14 20:17:18 -0400

    test: add test to check if operation type is correct

--- internal/model/operation_type_test.go
+++ internal/model/operation_type_test.go
@@ -6,7 +6,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestValidateOperationType(t *testing.T) {
+func TestOperationType_IDExists(t *testing.T) {
 	var cases = []struct {
 		id     int
 		result bool
@@ -37,6 +37,36 @@ func TestValidateOperationType(t *testing.T) {
 		t.Run("should validate operation type id", func(t *testing.T) {
 			result := ValidateOperationType(item.id)
 			assert.Equal(t, item.result, result)
+		})
+	}
+}
+
+func TestOperationType_CorrectIDToOperation(t *testing.T) {
+	var cases = []struct {
+		operationType OperationTypeId
+		result        int
+	}{
+		{
+			CASH_PURCHASE,
+			1,
+		},
+		{
+			INSTALLMENT_PURCHASE,
+			2,
+		},
+		{
+			WITHDRAWAL,
+			3,
+		},
+		{
+			PAYMENT,
+			4,
+		},
+	}
+
+	for _, item := range cases {
+		t.Run("should validate correct id to operation", func(t *testing.T) {
+			assert.Equal(t, int(item.operationType), item.result)
 		})
 	}
 }
